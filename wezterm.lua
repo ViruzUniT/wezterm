65f5b957e2e506243203bcc2d3946eddc2657f7b
ViruzUniT <viruz@unit.com>
2025-03-15 21:00:18 +0100

    fix: cleanup + color change

--- wezterm.lua
+++ wezterm.lua
@@ -1,34 +1,6 @@
--- local wezterm = require 'wezterm'
--- local keybinds = require 'keybinds'
--- local cfg = require 'config'
---
--- local config = {
---   enable_tab_bar = false,
---   default_prog = { 'powershell' },
---
---   font_dirs = { 'fonts' },
---   font_size = 12,
---   font = wezterm.font 'Iosevka NF',
---   --exit_behavior = "Hold"
---   color_scheme = 'rose-pine-moon',
--- }
---
--- for key, value in pairs(keybinds) do
---   config[key] = value
--- end
--- for key, value in pairs(cfg) do
---   config[key] = value
--- end
--- return config
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
--- local mux = wezterm.mux
--- This will hold the configuration.
 local config = wezterm.config_builder()
--- local gpus = wezterm.gui.enumerate_gpus()
--- config.webgpu_preferred_adapter = gpus[1]
--- config.front_end = "WebGpu"
 
 config.front_end = "OpenGL"
 config.max_fps = 144
@@ -37,23 +9,10 @@ config.animation_fps = 30
 config.cursor_blink_rate = 500
 config.term = "xterm-256color" -- Set the terminal type
 
--- config.font = wezterm.font("Iosevka Custom")
--- config.font = wezterm.font("Monocraft Nerd Font")
--- config.font = wezterm.font("FiraCode Nerd Font Mono")
--- config.font = wezterm.font("JetBrains Mono Regular")
 config.font_dirs = { "fonts" }
 config.font = wezterm.font("Iosevka NF")
 config.cell_width = 0.9
--- config.font = wezterm.font("Menlo Regular")
--- config.font = wezterm.font("Hasklig")
--- config.font = wezterm.font("Monoid Retina")
--- config.font = wezterm.font("InputMonoNarrow")
--- config.font = wezterm.font("mononoki Regular")
--- config.font = wezterm.font("Iosevka")
--- config.font = wezterm.font("M+ 1m")
--- config.font = wezterm.font("Hack Regular")
--- config.cell_width = 0.9
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.6
 config.prefer_egl = true
 config.font_size = 14.0
 
@@ -67,15 +26,6 @@ config.window_padding = {
 -- tabs
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
-
--- config.inactive_pane_hsb = {
--- 	saturation = 0.0,
--- 	brightness = 1.0,
--- }
-
--- This is where you actually apply your config choices
---
 
 -- color scheme toggling
 -- wezterm.on("toggle-colorscheme", function(window, pane)
@@ -180,16 +130,14 @@ config.keys = {
 -- config.color_scheme = "Cloud (terminal.sexy)"
 config.color_scheme = "rose-pine-moon"
 config.colors = {
-	-- background = '#3b224c',
+	-- background = "#3b224c",
 	-- background = "#181616", -- vague.nvim bg
 	-- background = "#080808", -- almost black
 	-- background = "#0c0b0f", -- dark purple
 	-- background = "#020202", -- dark purple
-	background = "#1e2326", -- everforest
-	--background = "#17151c", -- rose pine
-	-- background = "#16141a",
-	-- background = "#0e0e12", -- bright washed lavendar
-	-- background = 'rgba(59, 34, 76, 100%)',
+	-- background = "#1e2326", -- everforest
+	-- background = "#17151c", -- rose pine
+	background = "#16141a",
 	cursor_border = "#bea3c7",
 	-- cursor_fg = "#281733",
 	cursor_bg = "#87a2e8",
@@ -198,7 +146,6 @@ config.colors = {
 
 	tab_bar = {
 		background = "#0c0b0f",
-		-- background = "rgba(0, 0, 0, 0%)",
 		active_tab = {
 			bg_color = "#0c0b0f",
 			fg_color = "#bea3c7",
@@ -232,7 +179,8 @@ config.window_frame = {
 
 -- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_decorations = "NONE | RESIZE"
-config.default_prog = { "pwsh.exe", "-NoLogo" }
+-- config.default_prog = {"pwsh", "-NoLogo"}
+config.default_prog = { "bash" }
 config.initial_cols = 80
 config.underline_position = "-1px"
 config.underline_thickness = "2px"
@@ -240,17 +188,6 @@ config.underline_thickness = "2px"
 -- config.window_background_image_hsb = {
 -- 	brightness = 0.1,
 -- }
-
--- wezterm.on("gui-startup", function(cmd)
--- 	local args = {}
--- 	if cmd then
--- 		args = cmd.args
--- 	end
---
--- 	local tab, pane, window = mux.spawn_window(cmd or {})
--- 	-- window:gui_window():maximize()
--- 	-- window:gui_window():set_position(0, 0)
--- end)
 
 -- and finally, return the configuration to wezterm
 return config
